138aa3db44f9bb3216bb37fc41d6fb500bcb0e8e
Rafael Londoño <43898423+rafael492@users.noreply.github.com>
2021-10-10 00:29:13 -0500

    Add files via upload

---
+++ JavaApplication5/src/Saludo.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Rafael ING
+ */
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, aprendere a programar");
+    }
+    
+}
